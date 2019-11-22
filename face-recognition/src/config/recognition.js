b606b39429251cc0ff6fae7ad6a890a8f3df7cd6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-22 06:37:16 +0000

    Deploy midusi/proyecto2019 to github.com/midusi/proyecto2019.git:gh-pages

--- face-recognition/src/config/recognition.js
+++ face-recognition/src/config/recognition.js
@@ -1,5 +1,3 @@
-import _ from 'underscore'
-
 import angry from 'src-static/images/expressions/angry.png'
 import fearfull from 'src-static/images/expressions/fearfull.png'
 import happy from 'src-static/images/expressions/happy.png'
@@ -60,4 +58,4 @@ export const expressions = [{
   disabled: true,
 }]
 
-export const supportedExpressions = _.shuffle(expressions.filter(e => !e.disabled))
+export const supportedExpressions = expressions.filter(e => !e.disabled)
--- face-recognition/src/containers/Home/HomeContainer.jsx
+++ face-recognition/src/containers/Home/HomeContainer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
-import { Button, Divider,  } from 'semantic-ui-react'
+import { Divider,  } from 'semantic-ui-react'
 import Sound from 'react-sound'
 
 import homeBackground from 'src-static/images/home-background.jpg'
@@ -19,20 +19,27 @@ const HomeContainer = props => {
       <Sound url={yee} loop playStatus={Sound.status.PLAYING} />
       <div className='bg-image' style={{ backgroundImage: `url(${homeBackground})` }} />
       <div className='bg-text'>
-        <div className='cracked'>
+        {/*<div className='cracked'>
           <span>{ trans('home:title') }</span>
           <span>{ trans('home:title') }</span>
           <span>{ trans('home:title') }</span>
+        </div>*/}
+
+        <div id='text-slicer-gradient'>
+          {new Array(40).fill(1).map((_, i) => (
+            <div key={`slice-${i+1}`} className='text'>{trans('home:title')}</div>
+          ))}
         </div>
+
         <Divider hidden />
-        <Button
-          as={Link}
-          size='huge'
-          to='/game'
-          color='facebook'
-        >
-          { trans('home:option.recognize') }
-        </Button>
+        <Link to='/game'>
+          <button
+            type='button'
+            className='gradient-button'
+          >
+            { trans('home:option.recognize') }
+          </button>
+        </Link>
       </div>
     </div>
   )
